6b5cba98538ae1d7ba28fb830db409e8027295ac
Revito <revitomanik@gmail.com>
2023-12-04 01:25:27 +0800

    Halaman Pertama

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>U-TECH</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            background-image: url(bg.png);
+            background-size: cover;
+        }
+    </style>
+</head>
+<br><br><br><body> 
+    <div style="padding: 100px;">
+        <img src="logo.png" class="rounded mx-auto d-block" alt="..." width="250" height="250">
+        <br>
+        <div style="text-align: center;">
+            <a href="homepage.php" class="btn btn-dark btn-lg">CLICK HERE</a>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+</body></br></br></br>
+</html>
