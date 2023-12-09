85c569b0da8bad861190ceb22451baf65423a6bb
Revito <revitomanik@gmail.com>
2023-12-10 00:57:42 +0800

    database ya gengs

---
+++ db_utech.sql
@@ -0,0 +1,104 @@
+/*
+SQLyog Ultimate v12.5.1 (64 bit)
+MySQL - 10.4.28-MariaDB : Database - db_utech
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_utech` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `db_utech`;
+
+/*Table structure for table `tb_barang` */
+
+DROP TABLE IF EXISTS `tb_barang`;
+
+CREATE TABLE `tb_barang` (
+  `id_barang` int(10) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jenis_barang` enum('iPhone','Samsung','Oppo') NOT NULL,
+  `kategori_barang` enum('iOS','Android') NOT NULL,
+  `tahun_rilis` year(4) NOT NULL,
+  `harga_satuan` int(10) NOT NULL,
+  `stok_barang` int(5) NOT NULL,
+  PRIMARY KEY (`id_barang`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `tb_barang` */
+
+/*Table structure for table `tb_customer` */
+
+DROP TABLE IF EXISTS `tb_customer`;
+
+CREATE TABLE `tb_customer` (
+  `id_customer` int(10) NOT NULL,
+  `nama_customer` varchar(100) NOT NULL,
+  `telp_customer` varchar(15) NOT NULL,
+  PRIMARY KEY (`id_customer`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `tb_customer` */
+
+/*Table structure for table `tb_detail_transaksi` */
+
+DROP TABLE IF EXISTS `tb_detail_transaksi`;
+
+CREATE TABLE `tb_detail_transaksi` (
+  `id_detail_transaksi` int(5) NOT NULL,
+  `id_transaksi` int(5) NOT NULL,
+  `id_barang` int(10) NOT NULL,
+  `jumlah_barang` int(5) NOT NULL,
+  `subtotal` int(10) NOT NULL,
+  PRIMARY KEY (`id_detail_transaksi`),
+  KEY `id_transaksi` (`id_transaksi`),
+  KEY `id_barang` (`id_barang`),
+  CONSTRAINT `tb_detail_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `tb_transaksi` (`id_transaksi`),
+  CONSTRAINT `tb_detail_transaksi_ibfk_2` FOREIGN KEY (`id_barang`) REFERENCES `tb_barang` (`id_barang`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `tb_detail_transaksi` */
+
+/*Table structure for table `tb_pegawai` */
+
+DROP TABLE IF EXISTS `tb_pegawai`;
+
+CREATE TABLE `tb_pegawai` (
+  `id_pegawai` int(10) NOT NULL,
+  `nama_pegawai` varchar(100) NOT NULL,
+  `alamat_pegawai` varchar(255) NOT NULL,
+  `telp_pegawai` varchar(15) NOT NULL,
+  PRIMARY KEY (`id_pegawai`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `tb_pegawai` */
+
+/*Table structure for table `tb_transaksi` */
+
+DROP TABLE IF EXISTS `tb_transaksi`;
+
+CREATE TABLE `tb_transaksi` (
+  `id_transaksi` int(5) NOT NULL,
+  `id_customer` int(10) NOT NULL,
+  `id_pegawai` int(10) NOT NULL,
+  `tgl_transaksi` date NOT NULL,
+  `total` int(10) NOT NULL,
+  PRIMARY KEY (`id_transaksi`),
+  KEY `id_customer` (`id_customer`),
+  KEY `id_pegawai` (`id_pegawai`),
+  CONSTRAINT `tb_transaksi_ibfk_1` FOREIGN KEY (`id_customer`) REFERENCES `tb_customer` (`id_customer`),
+  CONSTRAINT `tb_transaksi_ibfk_2` FOREIGN KEY (`id_pegawai`) REFERENCES `tb_pegawai` (`id_pegawai`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `tb_transaksi` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
