ec697877771697a6dc05ecd901fae0beda7d5217
pyunzz <mngnovii@gmail.com>
2023-12-10 01:34:30 +0800

    baru update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>U-TECH</title>
+    <title> SELAMAT DATANG TOKO U-TECH</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
@@ -15,10 +15,29 @@
 <br><br><br><body> 
     <div style="padding: 100px;">
         <img src="Image/logo.png" class="rounded mx-auto d-block" alt="..." width="250" height="250">
+       
         <br>
-        <div style="text-align: center;">
-            <a href="homepage.php" class="btn btn-dark btn-lg">CLICK HERE</a>
-        </div>
+        <div class="container">
+ <form name="login" method="POST" action="aksi_login.php">
+  <h3 class="textJudul mb-5 mt-1">Login</h3>
+
+ <div class="mb-3">
+  <label for="exampleInputUserId" class="form-label textForm"> Masukan Id</label>
+  <input type="text" name="id" class="form-control" id="id" aria-describedby="idHelp"placeholder="Masukan Id">
+ </div>
+
+ <div class="mb-3">
+  <label for="exampleInputPassword1" class="form-label textForm">Password</label>
+  <input type="password" name="password" class="form-control" id="exampleInputPassword1"placeholder="Masukan Password">
+ </div>
+
+   
+</form>
+</div>
+ <script src="../js/bootstrap.js"></script>
+ <script src="../js/pooper.min.js"></script>
+
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
