74e4446aa36834a0dd1ee22dbfe84893b980bf2a
pyunzz <mngnovii@gmail.com>
2023-12-18 12:07:20 +0800

    y

--- pegawai/aksi_edit.php
+++ pegawai/aksi_edit.php
@@ -6,7 +6,7 @@ if(isset($_POST['id_pegawai'])) {
     $nama_pegawai = $_POST['nama_pegawai'];
     $alamat_pegawai = $_POST['alamat_pegawai'];
     $telp_pegawai = $_POST['telp_pegawai'];
-    $tggl_mulai = $_POST['telp_mulai'];
+    $tggl_mulai = $_POST['tggl_mulai'];
 
 
     $query = "UPDATE tb_pegawai SET nama_pegawai = '$nama_pegawai', alamat_pegawai = '$alamat_pegawai', telp_pegawai = '$telp_pegawai', tggl_mulai = '$tggl_mulai' WHERE id_pegawai = '$id_pegawai'";
